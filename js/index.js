fbd9f9b248da30c22351311b1b88db6c91db891a
Sérgio de Andrade Reichert <110432916+sergioreichert@users.noreply.github.com>
2023-02-15 16:30:52 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,19 +1,19 @@
-const botaoTrailer = document.querySelector(".botao-trailer");
-const botaoFecharModal = document.querySelector(".fechar-modal");
-const video = document.getElementById("video");
-const modal = document.querySelector(".modal");
+const botaoTrailer = document.querySelector(".botao-trailer")
+const botaoFecharModal = document.querySelector(".fechar-modal")
+const video = document.getElementById("video")
+const modal = document.querySelector(".modal")
 const linkDoVideo = video.src;
 
 function alternarModal(){
-	modal.classList.toggle("aberto");
+	modal.classList.toggle("aberto")
 }
 
 botaoTrailer.addEventListener("click", () => {
-	alternarModal();
-	video.setAttribute("src", linkDoVideo);
+	alternarModal()
+	video.setAttribute("src", linkDoVideo)
 });
 
 botaoFecharModal.addEventListener("click", () => {
-	alternarModal();
-	video.setAttribute("src", "");
+	alternarModal()
+	video.setAttribute("src", "")
 });
