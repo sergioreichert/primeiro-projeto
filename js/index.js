cc08da412a2548cf87bfecd3694525b22c4d7245
Sérgio de Andrade Reichert <110432916+sergioreichert@users.noreply.github.com>
2023-02-15 16:05:49 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 const botaoTrailer = document.querySelector(".botao-trailer");
 const botaoFecharModal = document.querySelector(".fechar-modal");
-const video = document.getElementById("video")
+const video = document.getElementById("video");
 const modal = document.querySelector(".modal");
 const linkDoVideo = video.src;
 
